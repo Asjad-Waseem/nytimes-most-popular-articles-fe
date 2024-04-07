eac3ca326f6600d05b62b21c29eae5f566ee831c
Asjad-Waseem <asjad.waseem123@gmail.com>
2024-04-07 14:53:25 +0500

    PR-0002-set-up-unit-tests-in-jest-react-testing-library-and-snapshot-testing

--- src/components/buttons/radio/types.ts
+++
@@ -1,7 +0,0 @@
-export interface RadioBtnProps {
-  className?: string;
-  options: string[];
-  selectedOption: string;
-  handleSelectedChange: React.ChangeEventHandler<HTMLInputElement>; // Change the type here
-  // handleSelectedChange: (e: React.MouseEvent<HTMLElement>, val: string) => void;
-}
